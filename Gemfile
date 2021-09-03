8e3617a64991a7ee587e78ea7d9bb564904f5a8c
Georjane <pzgeorjane@gmail.com>
2021-09-03 12:33:38 +0100

    Add hotel model tests

--- Gemfile
+++ Gemfile
@@ -22,7 +22,7 @@ gem 'rubocop', '>= 1.0', '< 2.0'
 
 # Reduces boot times through caching; required in config/boot.rb
 gem 'bootsnap', '>= 1.4.4', require: false
-
+gem 'figaro'
 # Use Rack CORS for handling Cross-Origin Resource Sharing (CORS), making cross-origin AJAX possible
 # gem 'rack-cors'
 
--- spec/models/hotel_spec.rb
+++ spec/models/hotel_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Hotel, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  # Association test
+  # it { should belong_to(:user) }
+  # Validation tests
+  it { should validate_presence_of(:title) }
+  it { should validate_presence_of(:description) }
+  it { should validate_presence_of(:image_url) }
+  it {
+    should validate_length_of(:title)
+  .is_at_least(3)   }
 end
